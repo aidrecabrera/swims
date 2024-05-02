a4ed33122b3ba4ccd1a51004d1d4bc1c014f41e9
Aidre Cabrera <aidrecabrera@gmail.com>
2024-05-03 01:08:43 +0800

    Refactor GSM module and remove unused code files

--- swims/swims.ino
+++ swims/swims.ino
@@ -1,16 +1,29 @@
+// helpers
 #include <Arduino.h>
 #include <EEPROM.h>
-#include <SoftwareSerial.h>
 #include <math.h>
 #include <ph4502c_sensor.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
+// protobuf & sensors
 #include "DFRobot_EC.h"
 #include "pb.h"
 #include "pb_decode.h"
 #include "pb_encode.h"
 #include "sensor_data.pb.h"
+
+// gsm module
+#include "Sim800L.h"
+#include <SoftwareSerial.h>
+
+#define RX 10
+#define TX 11
+
+Sim800L GSM(RX, TX);
+String text;
+String number;
+bool error;
 
 // pin definitions
 #define EC_PIN A1
@@ -29,36 +42,25 @@
 float voltage, ecValue, temperature = 0;
 String userNumber = "+639604377530";
 
+// objects
 DFRobot_EC ec;
 PH4502C_Sensor ph4502c(PH4502C_PH_PIN, PH4502C_TEMPERATURE_PIN, PH4502C_CALIBRATION,
                        PH4502C_READING_INTERVAL, PH4502C_READING_COUNT, ADC_RESOLUTION);
 OneWire oneWire(DS18B20_PIN);
-DallasTemperature sensors(&oneWire);
+DallasTemperature tempSensor(&oneWire);
+
+void notify(String message)
+{
+  error = GSM.sendSms(userNumber, message);
+}
 
 // sms module
 void sms(int action, String alertMessage = "")
 {
-  if (Serial.available() > 0)
+  if (GSM.available() > 0)
   {
-    char command = Serial.read();
-    switch (command)
-    {
-    case 0:
-      sim.sendMessage(alertMessage);
-      break;
-    case 1:
-      sim.receiveMessage();
-      break;
-    case 2:
-      sim.callNumber();
-      break;
-    case 3:
-      sim.getSignalStrength();
-      break;
-    case 4:
-      sim.getSimProvider();
-      break;
-    }
+    char command = GSM.read();
+    Serial.println("Enter the number to send the SMS to: " + command);
   }
 }
 
@@ -84,7 +86,7 @@ float readSalinity(float tempVal)
   { // time interval: 1s
     timepoint = millis();
     voltage = analogRead(EC_PIN) / 1024.0 * 5000; // read the voltage
-    ecValue = ec.readEC(voltage, tempVal);    // convert voltage to EC with temperature compensation
+    ecValue = ec.readEC(voltage, tempVal);        // convert voltage to EC with temperature compensation
     return ecValue;
   }
   ec.calibration(voltage, tempVal);
@@ -94,18 +96,21 @@ float readSalinity(float tempVal)
 // dissolved oxygen measurement
 float estimateDissolvedOxygen(float phLevel, float temperature, float salinity)
 {
-  float T = temperature + 273.15;  // celsius to kelvin
-  float S = salinity;               // salinity in parts per thousand (ppt)
-  float A = 457.88;                // constant a
-  float B = 2.00907;               // constant b
-  float C = 1737.62;               // constant c
-  float DO = (A * exp(B - C / T)) * (1.0 - 0.000025 * S); 
+  float T = temperature + 273.15; // celsius to kelvin
+  float S = salinity;             // salinity in parts per thousand (ppt)
+  float A = 457.88;               // constant a
+  float B = 2.00907;              // constant b
+  float C = 1737.62;              // constant c
+  float DO = (A * exp(B - C / T)) * (1.0 - 0.000025 * S);
 
   // pH correction factor
   float pHCorrection = 1.0;
-  if (phLevel < 7.0) {
+  if (phLevel < 7.0)
+  {
     pHCorrection = 1.0 - 0.032 * (7.0 - phLevel);
-  } else if (phLevel > 7.0) {
+  }
+  else if (phLevel > 7.0)
+  {
     pHCorrection = 1.0 - 0.032 * (phLevel - 7.0);
   }
   DO *= pHCorrection;
@@ -115,15 +120,12 @@ float estimateDissolvedOxygen(float phLevel, float temperature, float salinity)
 
 void setup()
 {
-  Serial.begin(9600);  // for sensors
-  // Serial.begin(14400); // for sms module
-  // sim800l v2
-  // sim.begin(14400);
-  // sim.setNumber(userNumber);
+  Serial.begin(9600); // for sensors
+  GSM.begin(4800);    // for GSM module
   // sensor initializations
   ph4502c.init();
   ec.begin();
-  sensors.begin();
+  tempSensor.begin();
   while (!Serial)
   {
     ; // necessary to wait for serial port connection
@@ -133,11 +135,11 @@ void setup()
 void loop()
 {
   // sensor readings
-  sensors.requestTemperatures();
+  tempSensor.requestTemperatures();
   SensorData sensorData = SensorData_init_zero;
-  sensorData.temperature = sensors.getTempCByIndex(0);                                                             // Temperature Measurement
+  sensorData.temperature = tempSensor.getTempCByIndex(0);                                                   // Temperature Measurement
   sensorData.pH = ph4502c.read_ph_level();                                                                  // pH Measurement
-  sensorData.salinity = readSalinity(sensorData.temperature);                                                                     // Salinity Measurement
+  sensorData.salinity = readSalinity(sensorData.temperature);                                               // Salinity Measurement
   sensorData.dOxygen = estimateDissolvedOxygen(sensorData.pH, sensorData.temperature, sensorData.salinity); // Dissolved Oxygen Measurement
 
   // protobuf encoding
