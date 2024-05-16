1f13f5782afe84c0e8ada9ce670b980cbe652b68
Svene <hello.svene@gmail.com>
2024-05-17 01:35:53 +0800

    refactor

--- swims-web/src/components/component/dashboard.tsx
+++ swims-web/src/components/component/dashboard.tsx
@@ -1,7 +1,10 @@
 // App.js
+import { supabase } from "@/services/supabaseClient";
 import { format } from "date-fns";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
+  Area,
+  AreaChart,
   CartesianGrid,
   Legend,
   Line,
@@ -14,21 +17,38 @@ import {
 import { Card, CardContent, CardHeader } from "../ui/card";
 import RecentMetricsCard from "./recent-metric-card";
 
+export type TSwims = {
+  id?: string;
+  temperature?: string;
+  ph?: string;
+  dissolved_oxygen?: string;
+  salinity?: string;
+  timestamp?: any;
+};
+
 const Dashboard = () => {
   const [startDate, setStartDate] = useState("");
   const [endDate, setEndDate] = useState("");
+  const [data, setData] = useState<TSwims[]>([]);
 
-  // Mock data for testing purposes
-  const data = [
-    {
-      id: "33679",
-      temperature: "33",
-      ph: "6",
-      dissolved_oxygen: "11.33",
-      salinity: "0.09",
-      timestamp: "2024-05-13 09:03:10+00",
-    },
-  ];
+  const fetchSwimsData = async () => {
+    const { data: sensor_data, error } = await supabase
+      .from("sensor_data")
+      .select("*")
+      .range(1000, 7000);
+    if (error) {
+      console.error("Error fetching data:", error.message);
+    }
+    return sensor_data;
+  };
+
+  useEffect(() => {
+    fetchSwimsData().then((sensor_data) => {
+      if (sensor_data) {
+        setData(sensor_data);
+      }
+    });
+  }, []);
 
   const filteredData = data.filter((item) => {
     const itemDate = new Date(item.timestamp);
@@ -47,34 +67,64 @@ const Dashboard = () => {
 
   const latestMeasurement = data[data.length - 1];
 
+  function calculateChange(currentMeasurement: number, measurements: any[]) {
+    const oneHourAgo = new Date();
+    oneHourAgo.setHours(oneHourAgo.getHours() - 1);
+
+    const lastHourMeasurement = measurements.find(
+      (measurement) => new Date(measurement.timestamp) <= oneHourAgo
+    );
+
+    if (!lastHourMeasurement) {
+      return 0;
+    }
+
+    const change =
+      ((currentMeasurement - parseFloat(lastHourMeasurement.temperature!)) /
+        parseFloat(lastHourMeasurement.temperature!)) *
+      100;
+
+    return change.toFixed(2) as any;
+  }
   return (
     <div className="container p-4 mx-auto">
-      <div className="grid grid-cols-1 gap-4 mb-4 sm:grid-cols-2 lg:grid-cols-4">
-        <RecentMetricsCard
-          key={`${latestMeasurement.id}-temperature`}
-          title="Temperature"
-          count={parseFloat(latestMeasurement.temperature)}
-          change={0} // replace with actual change value
-        />
-        <RecentMetricsCard
-          key={`${latestMeasurement.id}-ph`}
-          title="pH"
-          count={parseFloat(latestMeasurement.ph)}
-          change={0} // replace with actual change value
-        />
-        <RecentMetricsCard
-          key={`${latestMeasurement.id}-dissolved_oxygen`}
-          title="Dissolved Oxygen"
-          count={parseFloat(latestMeasurement.dissolved_oxygen)}
-          change={0} // replace with actual change value
-        />
-        <RecentMetricsCard
-          key={`${latestMeasurement.id}-salinity`}
-          title="Salinity"
-          count={parseFloat(latestMeasurement.salinity)}
-          change={0} // replace with actual change value
-        />
-      </div>
+      {latestMeasurement && (
+        <div className="grid grid-cols-1 gap-4 mb-4 sm:grid-cols-2 lg:grid-cols-4">
+          <RecentMetricsCard
+            key={`${latestMeasurement.id}-temperature`}
+            title="Temperature"
+            count={parseFloat(latestMeasurement.temperature!)}
+            change={calculateChange(
+              parseFloat(latestMeasurement.temperature!),
+              data
+            )}
+          />
+          <RecentMetricsCard
+            key={`${latestMeasurement.id}-ph`}
+            title="pH"
+            count={parseFloat(latestMeasurement.ph!)}
+            change={calculateChange(parseFloat(latestMeasurement.ph!), data)}
+          />
+          <RecentMetricsCard
+            key={`${latestMeasurement.id}-dissolved_oxygen`}
+            title="Dissolved Oxygen"
+            count={parseFloat(latestMeasurement.dissolved_oxygen!)}
+            change={calculateChange(
+              parseFloat(latestMeasurement.dissolved_oxygen!),
+              data
+            )}
+          />
+          <RecentMetricsCard
+            key={`${latestMeasurement.id}-salinity`}
+            title="Salinity"
+            count={parseFloat(latestMeasurement.salinity!)}
+            change={calculateChange(
+              parseFloat(latestMeasurement.salinity!),
+              data
+            )}
+          />
+        </div>
+      )}
       <div className="flex flex-col items-center mb-4 sm:flex-row">
         <label htmlFor="startDate" className="mb-2 mr-2 sm:mb-0">
           Start Date:
@@ -98,111 +148,23 @@ const Dashboard = () => {
           className="px-2 py-1 border border-gray-300 rounded"
         />
       </div>
-      {/* For combined charts */}
-      <div>
-        <div className="my-4">
-          <Card>
-            <CardHeader>
-              <h2 className="mb-4 text-xl font-bold">Combined Chart</h2>
-            </CardHeader>
-            <CardContent>
-              <ResponsiveContainer width="100%" height={300}>
-                <LineChart data={filteredData}>
-                  <CartesianGrid strokeDasharray="3 3" />
-                  <XAxis
-                    dataKey="timestamp"
-                    tickFormatter={(value: any) =>
-                      format(new Date(value), "HH:mm")
-                    }
-                  />
-                  <YAxis />
-                  <Tooltip
-                    labelFormatter={(value: any) =>
-                      format(new Date(value), "yyyy-MM-dd HH:mm")
-                    }
-                  />
-                  <Legend />
-                  <Line
-                    type="monotone"
-                    dataKey="temperature"
-                    stroke="#8884d8"
-                  />
-                  <Line type="monotone" dataKey="ph" stroke="#82ca9d" />
-                  <Line
-                    type="monotone"
-                    dataKey="dissolved_oxygen"
-                    stroke="#ffc658"
-                  />
-                  <Line type="monotone" dataKey="salinity" stroke="#ff7300" />
-                </LineChart>
-              </ResponsiveContainer>
-            </CardContent>
-          </Card>
-        </div>
-      </div>
-      {/* For individual charts */}
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+      {/* combined charts */}
+      <div className="my-4">
         <Card>
           <CardHeader>
-            <h2 className="mb-4 text-xl font-bold">Temperature</h2>
+            <h2 className="mb-4 text-xl font-bold">Combined Chart</h2>
           </CardHeader>
           <CardContent>
             <ResponsiveContainer width="100%" height={300}>
-              <LineChart data={filteredData}>
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis
-                  dataKey="timestamp"
-                  tickFormatter={(value: any) =>
-                    format(new Date(value), "HH:mm")
-                  }
-                />
-                <YAxis />
-                <Tooltip
-                  labelFormatter={(value: any) =>
-                    format(new Date(value), "yyyy-MM-dd HH:mm")
-                  }
-                />
-                <Legend />
-                <Line type="monotone" dataKey="temperature" stroke="#8884d8" />
-              </LineChart>
-            </ResponsiveContainer>
-          </CardContent>
-        </Card>
-
-        <Card>
-          <CardHeader>
-            <h2 className="mb-4 text-xl font-bold">pH</h2>
-          </CardHeader>
-          <CardContent>
-            <ResponsiveContainer width="100%" height={300}>
-              <LineChart data={filteredData}>
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis
-                  dataKey="timestamp"
-                  tickFormatter={(value: any) =>
-                    format(new Date(value), "HH:mm")
-                  }
-                />
-                <YAxis />
-                <Tooltip
-                  labelFormatter={(value: any) =>
-                    format(new Date(value), "yyyy-MM-dd HH:mm")
-                  }
-                />
-                <Legend />
-                <Line type="monotone" dataKey="ph" stroke="#82ca9d" />
-              </LineChart>
-            </ResponsiveContainer>
-          </CardContent>
-        </Card>
-
-        <Card>
-          <CardHeader>
-            <h2 className="mb-4 text-xl font-bold">Dissolved Oxygen</h2>
-          </CardHeader>
-          <CardContent>
-            <ResponsiveContainer width="100%" height={300}>
-              <LineChart data={filteredData}>
+              <LineChart
+                margin={{
+                  top: 0,
+                  right: 0,
+                  left: -25,
+                  bottom: 0,
+                }}
+                data={filteredData}
+              >
                 <CartesianGrid strokeDasharray="3 3" />
                 <XAxis
                   dataKey="timestamp"
@@ -218,43 +180,158 @@ const Dashboard = () => {
                 />
                 <Legend />
                 <Line
+                  dot={false}
+                  strokeWidth={3}
+                  type="monotone"
+                  dataKey="temperature"
+                  stroke="#8884d8"
+                />
+                <Line
+                  dot={false}
+                  strokeWidth={3}
+                  type="monotone"
+                  dataKey="ph"
+                  stroke="#82ca9d"
+                />
+                <Line
+                  dot={false}
+                  strokeWidth={3}
                   type="monotone"
                   dataKey="dissolved_oxygen"
                   stroke="#ffc658"
                 />
+                <Line
+                  dot={false}
+                  strokeWidth={3}
+                  type="monotone"
+                  dataKey="salinity"
+                  stroke="#ff7300"
+                />
               </LineChart>
             </ResponsiveContainer>
           </CardContent>
         </Card>
-        <Card>
-          <CardHeader>
-            <h2 className="mb-4 text-xl font-bold">Salinity</h2>
-          </CardHeader>
-          <CardContent>
-            <ResponsiveContainer width="100%" height={300}>
-              <LineChart data={filteredData}>
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis
-                  dataKey="timestamp"
-                  tickFormatter={(value: any) =>
-                    format(new Date(value), "HH:mm")
-                  }
-                />
-                <YAxis />
-                <Tooltip
-                  labelFormatter={(value: any) =>
-                    format(new Date(value), "yyyy-MM-dd HH:mm")
-                  }
-                />
-                <Legend />
-                <Line type="monotone" dataKey="salinity" stroke="#ff7300" />
-              </LineChart>
-            </ResponsiveContainer>
-          </CardContent>
-        </Card>
+      </div>
+      {/* For individual charts */}
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-6">
+        <IndividualChart filteredData={filteredData} />
       </div>
     </div>
   );
 };
 
 export default Dashboard;
+
+type IndividualChartProps = {
+  filteredData: TSwims[];
+};
+
+const IndividualChart: React.FC<IndividualChartProps> = ({ filteredData }) => {
+  const cardData = [
+    {
+      title: "Temperature",
+      dataKey: "temperature",
+      stroke: "#8884d8",
+    },
+    {
+      title: "pH",
+      dataKey: "ph",
+      stroke: "#82ca9d",
+    },
+    {
+      title: "Dissolved Oxygen",
+      dataKey: "dissolved_oxygen",
+      stroke: "#ffc658",
+    },
+    {
+      title: "Salinity",
+      dataKey: "salinity",
+      stroke: "#ff7300",
+    },
+  ];
+
+  return cardData.map((card) => (
+    <Card className="col-span-3">
+      <CardHeader>
+        <h2 className="mb-4 text-xl font-bold">{card.title}</h2>
+      </CardHeader>
+      <CardContent>
+        {card.dataKey === "ph" ? (
+          <ResponsiveContainer width="100%" height={300}>
+            <AreaChart
+              margin={{
+                top: 0,
+                right: 0,
+                left: -25,
+                bottom: 0,
+              }}
+              data={filteredData.map((item) => ({
+                ...item,
+                ph: (Number(item.ph ?? 0) - 5 > 14
+                  ? 14
+                  : Number(item.ph ?? 0) - 5
+                ).toFixed(2),
+              }))}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis
+                dataKey="timestamp"
+                tickFormatter={(value: any) => format(new Date(value), "HH:mm")}
+              />
+              <YAxis domain={[0, 20]} />
+              <Tooltip
+                labelFormatter={(value: any) =>
+                  format(new Date(value), "yyyy-MM-dd HH:mm")
+                }
+              />
+              <Legend />
+              <defs>
+                <linearGradient id="splitColor" x1="0" y1="0" x2="0" y2="1">
+                  <stop offset="0.5" stopColor="green" stopOpacity={1} />
+                  <stop offset="0.5" stopColor="red" stopOpacity={1} />
+                </linearGradient>
+              </defs>
+              <Area
+                type="basisClosed"
+                dataKey={card.dataKey}
+                stroke={card.stroke}
+                fill="url(#splitColor)"
+              />
+            </AreaChart>
+          </ResponsiveContainer>
+        ) : (
+          <ResponsiveContainer width="100%" height={300}>
+            <AreaChart
+              margin={{
+                top: 0,
+                right: 0,
+                left: -25,
+                bottom: 0,
+              }}
+              data={filteredData}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis
+                dataKey="timestamp"
+                tickFormatter={(value: any) => format(new Date(value), "HH:mm")}
+              />
+              <YAxis />
+              <Tooltip
+                labelFormatter={(value: any) =>
+                  format(new Date(value), "yyyy-MM-dd HH:mm")
+                }
+              />
+              <Legend />
+              <Area
+                type="basisClosed"
+                dataKey={card.dataKey}
+                stroke={card.stroke}
+                fill={card.stroke}
+              />
+            </AreaChart>
+          </ResponsiveContainer>
+        )}
+      </CardContent>
+    </Card>
+  ));
+};
--- swims-web/src/components/component/recent-metric-card.tsx
+++ swims-web/src/components/component/recent-metric-card.tsx
@@ -1,5 +1,3 @@
-import { Activity } from "lucide-react";
-
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 interface RecentMetricsCardProps {
@@ -17,12 +15,11 @@ export default function RecentMetricsCard({
     <Card className="flex-grow">
       <CardHeader className="flex flex-row items-center justify-between pb-2 space-y-0">
         <CardTitle className="text-sm font-medium">{title}</CardTitle>
-        <Activity className="w-4 h-4 text-muted-foreground" />
       </CardHeader>
       <CardContent>
         <div className="text-2xl font-bold">{count}</div>
         <p className="text-xs text-muted-foreground">
-          {change} since last hour
+          {change}% change since last hour
         </p>
       </CardContent>
     </Card>
